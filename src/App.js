fe03a35988dc473f731ff1f398d99a72d3058beb
DerrickDarku <derrickalberto@ymail.com>
2020-03-09 11:32:01 +0000

    changed background color to crimson

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import './App.css';
+import React, { Component } from 'react'
+
+ class App extends Component {
+   constructor(props){
+     super(props);
+     this.state={
+       users:[{
+         name:"Derrick Darku",
+         email:"derrickdarku@gmaul.com",
+         gen:3
+       },
+       {
+        name:"Joseph Sundane",
+        email:"Joe@ymail.com",
+        gen:5
+      },
+      {
+        name:"Sarah Graham",
+        email:"SaraG@yahoo.com",
+        gen:9
+      }
+
+       ],
+       name:"",
+       email:"",
+       gen:""
+     };
+
+   }
+
+
+
+
+   handleSubmit=(e) => {
+     e.preventDefault();
+     const newUser= {
+       name:this.state.name,
+       email:this.state.email,
+       gen:this.state.gen
+     }
+     this.setState({
+       users:[...this.state.users,newUser],
+       name:"",
+       email:"",
+       gen:""
+     })
+   }
+
+   handleChange=(e) =>{
+     this.setState({
+       [e.target.name]:e.target.value
+     });
+   }
+
+
+  render() {
+    return (
+      
+      <div className="App">
+        <form onSubmit ={this.handleSubmit}className="form" >
+          <div className="form-input">
+            <label>Name</label>
+            <input type="text" name="name" value={this.state.name} onChange={this.handleChange} />
+          </div>
+          <div className="form-input">
+            <label>Email</label>
+            <input type="email" name="email"value={this.state.email} onChange={this.handleChange}/>
+          </div>
+          <div className="form-input">
+            <label>Gen</label>
+            <input type="number" name="gen" value={this.state.gen}onChange={this.handleChange}/>
+          </div>
+            <button type="submit">Add User</button>
+        </form>
+        {this.state.users.map(user=>{
+          return(
+            <div className="info">
+              <h3>Name:{user.name}</h3>
+              <p>Email:{user.email}</p>
+              <h3>Gen{user.gen}</h3>
+            </div>
+          );
+        })}
+      </div>
+    )
+  }
+}
+export default App;
